c00fa4c206de8462453aa8f84c9292ac9131d901
Samuel Schuffler <sschuffler1332@jh-mba158-22.local>
2019-03-06 14:55:04 -0600

    Initial Commit

---
+++ mobileMapper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mobileMapper
+//
+//  Created by Samuel Schuffler on 3/6/19.
+//  Copyright © 2019 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
